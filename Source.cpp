d896925ffec1001b089530e4a04213ab59dc3d47
ArseniyMyasnik <arseniym148@gmail.com>
2022-10-30 21:17:34 +0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -27,62 +27,62 @@ int main() {
 	}
 	deskk(desk);
 	int f1_x, f1_y, f2_x, f2_y;
-	XY1:
-	cout << "Введите координату x для первой фигуры(считая доску I четвертью системы координат): ";
+XY1:
+	cout << "Vvedite koordinatu x dlya pervoj figury(schitaya dosku I chetvert'yu sistemy koordinat): ";
 	cin >> f1_x;
-	cout << "Введите координату y для первой фигуры(считая доску I четвертью системы координат): ";
+	cout << "Vvedite koordinatu y dlya pervoj figury(schitaya dosku I chetvert'yu sistemy koordinat): ";
 	cin >> f1_y;
 	if ((f1_x) > 8 || (f1_x) < 1 || (f1_y) > 8 || (f1_y) < 1) {
-		cout << "Введите цифру от 1 до 8!" << endl;
+		cout << "Vvedite cifru ot 1 do 8!" << endl;
 		goto XY1;
 	}
 FT1:
-	cout << "Введите тип фигуры, для этого выберите цифру" << endl << "1 - ферзь" << endl << "2 - ладья" << endl << "3 - слон" << endl << "4 - конь" << endl;
+	cout << "Vvedite tip figury, dlya etogo vyberite cifru" << endl << "1 - ferz'" << endl << "2 - lad'ya" << endl << "3 - slon" << endl << "4 - kon'" << endl;
 	cin >> f1_t;
 	if ((f1_t) > 4 || (f1_t) < 1) {
-		cout << "Введите цифру в диапазоне от 1 до 4!" << endl;
+		cout << "Vvedite cifru v diapazone ot 1 do 4!" << endl;
 		goto FT1;
 	}
-	XY2:
-	cout << "Введите координату x для второй фигуры(считая доску I четвертью системы координат): ";
+XY2:
+	cout << "Vvedite koordinatu x dlya vtoroj figury(schitaya dosku I chetvert'yu sistemy koordinat): ";
 	cin >> f2_x;
-	cout << "Введите координату y для второй фигуры(считая доску I четвертью системы координат): ";
+	cout << "Vvedite koordinatu y dlya vtoroj figury(schitaya dosku I chetvert'yu sistemy koordinat): ";
 	cin >> f2_y;
 	if ((f2_x) > 8 || (f2_x) < 1 || (f2_y) > 8 || (f2_y) < 1) {
-		cout << "Введите цифру от 1 до 8!" << endl;
+		cout << "Vvedite cifru ot 1 do 8!" << endl;
 		goto XY2;
 	}
 FT2:
-	cout << "Введите тип фигуры, для этого выберите цифру" << endl << "1 - ферзь" << endl << "2 - ладья" << endl << "3 - слон" << endl << "4 - конь" << endl;
+	cout << "Vvedite tip figury, dlya etogo vyberite cifru" << endl << "1 - ferz'" << endl << "2 - lad'ya" << endl << "3 - slon" << endl << "4 - kon'" << endl;
 	cin >> f2_t;
 	if ((f2_t) > 4 || (f2_t) < 1) {
-		cout << "Введите цифру в диапазоне от 1 до 4!" << endl;
+		cout << "Vvedite cifru v diapazone ot 1 do 4!" << endl;
 		goto FT2;
 	}
 
 	if (f1_t == 1) {
-		desk[f1_y][f1_x] = "  Ф";
+		desk[f1_y][f1_x] = "  F";
 	}
 	else if (f1_t == 2) {
-		desk[f1_y][f1_x] = "  Л";
+		desk[f1_y][f1_x] = "  L";
 	}
 	else if (f1_t == 3) {
-		desk[f1_y][f1_x] = "  С";
+		desk[f1_y][f1_x] = "  S";
 	}
 	else if (f1_t == 4) {
-		desk[f1_y][f1_x] = "  К";
+		desk[f1_y][f1_x] = "  K";
 	}
 	if (f2_t == 1) {
-		desk[f2_y][f2_x] = "  Ф";
+		desk[f2_y][f2_x] = "  F";
 	}
 	else if (f2_t == 2) {
-		desk[f2_y][f2_x] = "  Л";
+		desk[f2_y][f2_x] = "  L";
 	}
 	else if (f2_t == 3) {
-		desk[f2_y][f2_x] = "  С";
+		desk[f2_y][f2_x] = "  S";
 	}
 	else if (f2_t == 4) {
-		desk[f2_y][f2_x] = "  К";
+		desk[f2_y][f2_x] = "  K";
 	}
 	deskk(desk);
 
@@ -90,91 +90,91 @@ FT2:
 	cout << f1_y % 2 << endl;
 	if (f1_y % 2 == 0 && f1_x % 2 == 0) {
 		f1_p = 0;
-		cout << "Первая фигура стоит на черной клетке" << endl;
+		cout << "Pervaya figura stoit na chernoj kletke" << endl;
 	}
 	else if (f1_y % 2 == 0 && f1_x % 2 != 0) {
 		f1_p = 1;
-		cout << "Первая фигура стоит на белой клетке" << endl;
+		cout << "Pervaya figura stoit na beloj kletke" << endl;
 	}
 	else if (f1_y % 2 != 0 && f1_x % 2 == 0) {
 		f1_p = 1;
-		cout << "Первая фигура стоит на белой клетке" << endl;
+		cout << "Pervaya figura stoit na beloj kletke" << endl;
 	}
 	else if (f1_y % 2 != 0 && f1_x % 2 != 0) {
 		f1_p = 0;
-		cout << "Первая фигура стоит на черной клетке" << endl;
+		cout << "Pervaya figura stoit na chernoj kletke" << endl;
 	}
 
 	if (f2_y % 2 == 0 && f2_x % 2 == 0) {
 		f2_p = 0;
-		cout << "Вторая фигура стоит на черной клетке" << endl;
+		cout << "Vtoraya figura stoit na chernoj kletke" << endl;
 	}
 	else if (f2_y % 2 == 0 && f2_x % 2 != 0) {
 		f2_p = 1;
-		cout << "Вторая фигура стоит на белой клетке" << endl;
+		cout << "Vtoraya figura stoit na beloj kletke" << endl;
 	}
 	else if (f2_y % 2 != 0 && f2_x % 2 == 0) {
 		f2_p = 1;
-		cout << "Вторая фигура стоит на белой клетке" << endl;
+		cout << "Vtoraya figura stoit na beloj kletke" << endl;
 	}
 	else if (f2_y % 2 != 0 && f2_x % 2 != 0) {
 		f2_p = 0;
-		cout << "Вторая фигура стоит на черной клетке" << endl;
+		cout << "Vtoraya figura stoit na chernoj kletke" << endl;
 	}
 
 	if (f1_p == f2_p) {
-		cout << "Обе фигуры стоят на клетках одинакового цвета" << endl;
+		cout << "Obe figury stoyat na kletkah odinakovogo cveta" << endl;
 	}
 	else {
-		cout << "Обе фигуры стоят на клетках разного цвета" << endl;
+		cout << "Obe figury stoyat na kletkah raznogo cveta" << endl;
 	}
 
 	if (f1_t == 1) {
 		for (int i = 0; i < 8; i++) {
 			if ((f2_x == f1_x + i) && (f2_y == f1_y + i) || (f2_x == f1_x - i) && (f2_y == f1_y - i) || (f2_x == f1_x + i) && (f2_y == f1_y - i) || (f2_x == f1_x - i) && (f2_y == f1_y + i)) {
-				cout << "Вторая фигура находится под ударом ферзя";
+				cout << "Vtoraya figura nahoditsya pod udarom ferzya";
 				goto IF1T;
 			}
 		}
 		if ((f1_x == f2_x) || (f1_y == f2_y)) {
-			cout << "Вторая фигура находится под ударом ферзя";
+			cout << "Vtoraya figura nahoditsya pod udarom ferzya";
 		}
 		else {
-			cout << "Вторая фигура не находится под ударом ферзя";
+			cout << "Vtoraya figura ne nahoditsya pod udarom ferzya";
 		}
 
 	}
-	IF1T:
+IF1T:
 	if (f1_t == 2) {
 		if ((f1_x == f2_x) || (f1_y == f2_y)) {
-			cout << "Вторая фигура находится под ударом ладьи";
+			cout << "Vtoraya figura nahoditsya pod udarom lad'i";
 		}
 		else {
-			cout << "Вторая фигура не находится под ударом ладьи";
+			cout << "Vtoraya figura ne nahoditsya pod udarom lad'i";
 		}
 	}
 	if (f1_t == 3) {
 		for (int i = 0; i < 8; i++) {
 			if ((f2_x == f1_x + i) && (f2_y == f1_y + i) || (f2_x == f1_x - i) && (f2_y == f1_y - i) || (f2_x == f1_x + i) && (f2_y == f1_y - i) || (f2_x == f1_x - i) && (f2_y == f1_y + i)) {
-				cout << "Вторая фигура находится под ударом ферзя";
+				cout << "Vtoraya figura nahoditsya pod udarom ferzya";
 			}
 		}
 	}
 	if (f1_t == 4) {
 		if ((f1_x + 2 == f2_x) && (f1_y + 1) == f2_y || (f1_x + 2 == f2_x) && (f1_y - 1) == f2_y) {
-			cout << "Вторая фигура находится под ударом коня";
+			cout << "Vtoraya figura nahoditsya pod udarom konya";
 		}
 		if ((f1_x - 2 == f2_x) && (f1_y + 1) == f2_y || (f1_x - 2 == f2_x) && (f1_y - 1) == f2_y) {
-			cout << "Вторая фигура находится под ударом коня";
+			cout << "Vtoraya figura nahoditsya pod udarom konya";
 		}
 		if ((f1_y + 2 == f2_y) && (f1_x + 1) == f2_x || (f1_y + 2 == f2_y) && (f1_x - 1) == f2_x) {
-			cout << "Вторая фигура находится под ударом коня";
+			cout << "Vtoraya figura nahoditsya pod udarom konya";
 		}
 		if ((f1_y - 2 == f2_y) && (f1_x + 1) == f2_x || (f1_y - 2 == f2_y) && (f1_x - 1) == f2_x) {
-			cout << "Вторая фигура находится под ударом коня";
+			cout << "Vtoraya figura nahoditsya pod udarom konya";
 		}
 		else {
-			cout << "Вторая фигура не находится под ударом коня";
+			cout << "Vtoraya figura ne nahoditsya pod udarom konya";
 		}
 	}
 }
